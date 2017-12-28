b67b886ec83a80184ed285ac6a75139cca9c3206
Ahmed Alaa Abd Alhy <canvip2011@gmail.com>
2017-12-28 23:30:13 +0000

    test v 3

--- conf.sh
+++ conf.sh
@@ -44,14 +44,16 @@ WKHTMLTOX_X32=https://downloads.wkhtmltopdf.org/0.12/0.12.1/wkhtmltox-0.12.1_lin
 #sudo npm install -g less less-plugin-clean-css -y && sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
-sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
+#sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
+sudo su - postgres -c "psql -c \"CREATE ROLE odoo WITH PASSWORD 'odoo' SUPERUSER;\""
+sudo su - postgres -c "psql -c \"ALTER ROLE odoo WITH LOGIN;\""
 sudo su - postgres -c "psql -c \"update pg_database set datallowconn = TRUE where datname = 'template0';\""
-sudo su - postgres -c "psql -c \"drop database template1;\""
-sudo su - postgres -c "psql -c \"create database template1 with template = template0 encoding = 'unicode';\""
+sudo su - postgres -c "psql -d template0 -c \"drop database template1;\""
+sudo su - postgres -c "psql -d template0 -c \"create database template1 with template = template0 encoding = 'unicode';\""
 sudo su - postgres -c "psql -d template0 -c \"update pg_database set datistemplate = TRUE where datname = 'template1';\""
 sudo su - postgres -c "psql -c  \"\c template1;\""
 sudo su - postgres -c "psql -d template0 -c \" vacuum freeze;\""
-								
+							
 #--------------------------------------------------
 # Install Wkhtmltopdf if needed
 #--------------------------------------------------
